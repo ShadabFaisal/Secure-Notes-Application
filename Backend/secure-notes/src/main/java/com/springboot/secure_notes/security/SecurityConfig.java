ae7265fbaaecf35365965ff36bdee4ab2ab92038
Shadab Faisal <shadabfaisal91@gmail.com>
2025-02-27 19:36:41 +0530

    Configured InMemory Authentication

--- Backend/secure-notes/src/main/java/com/springboot/secure_notes/security/SecurityConfig.java
+++ Backend/secure-notes/src/main/java/com/springboot/secure_notes/security/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,16 +21,36 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) ->
-                requests
-                        .requestMatchers("/contact").permitAll()
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/admin").denyAll()
-                        .requestMatchers("/admin/**").denyAll()
-                        .anyRequest().authenticated());
+                requests.anyRequest().authenticated());
+        http.csrf(csrf->csrf.disable());
         //http.formLogin(Customizer.withDefaults());
-        http.sessionManagement(session->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(Customizer.withDefaults());
+
+//      This step is to make the session stateless i.e. no data is saved on the client side.
+//        http.sessionManagement(session->
+//                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager=new InMemoryUserDetailsManager();
+        if(!manager.userExists("user1")){
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password1")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if(!manager.userExists("admin")){
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
     }
 }
